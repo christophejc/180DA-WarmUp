79f4295a24e13d3f9922583bab0183efacc1308b
christophejc <christophejc24@g.ucla.edu>
2024-01-24 18:34:50 -0800

    audio

--- Lab2/speech.py
+++ Lab2/speech.py
@@ -12,7 +12,7 @@ while True:
             recognizer.adjust_for_ambient_noise(mic, duration=0.2)
             audio = recognizer.listen(mic)
 
-            text = recognizer.recognize_google_cloud(audio)
+            text = recognizer.recognize_google(audio)
             text = text.lower()
 
             print(f"Recognized {text}")
