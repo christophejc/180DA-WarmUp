ef202521bee46f1653481af69841e8758e959d1d
christophejc <christophejc24@g.ucla.edu>
2024-02-01 09:12:53 -0800

    multiplayer game

--- Lab3/centralRPS.py
+++ Lab3/centralRPS.py
@@ -1,14 +1,21 @@
 import paho.mqtt.client as mqtt
 import numpy as np
 
-# Define callbacks - functions that run when events happen.
+# THIS ACTS AS THE BROKER FOR THE INDIVIDUAL PLAYERS 
+brokerAddress = "host"
+brokerPort = 1883
+topic = "RPS"
+
+#storing the players moves
+moves = {}
+
 
 # Callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
     print("Connection returned result: " + str(rc))
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    # client.subscribe("ece180d/test")
+    client.subscribe(topic)
 
 # Callback of the client when it disconnects.
 def on_disconnect(client, userdata, rc):
@@ -20,8 +27,28 @@ def on_disconnect(client, userdata, rc):
 # Default message callback.
 # (won’t be used if only publishing, but can still exist)
 def on_message(client, userdata, message):
-    print('Received message: "' + str(message.payload) + '" on topic "' +
-          message.topic + '" with QoS ' + str(message.qos))
+    players, currentMove = message.payload.decode().split(",")
+    moves[players] = currentMove
+    
+    if len(moves) == 2:
+       winner() 
+
+def winner ():
+    p1, move1 = list(moves.items())[0]
+    p2, move2 = list(moves.items())[1]
+
+    if move1 == move2:
+        result = "its a tie!"
+    elif (move1 == "r" and move2 == "s") or (move1 == "p" and move2 == "r") or (move1 == "s" and move2 == "p"):
+        result = f"{p1} wins!"
+    else:
+        result = f"{p2} 2 wins!"
+
+    print(result)
+
+    # Reset player moves for the next round
+    moves.clear()
+
 
 # Create a client instance.
 client = mqtt.Client()
@@ -41,14 +68,14 @@ client.connect_async('mqtt.eclipseprojects.io')
 # Call one of the loop*() functions to maintain network traffic flow with the broker.
 client.loop_start()
 
-# Use subscribe() to subscribe to a topic and receive messages.
+try:
+    while True:
+        pass
 
-# Use publish() to publish messages to the broker.
-# Payload must be a string, bytearray, int, float, or None.
-print('Publishing...')
-for i in range(10):
-    client.publish("ece180d/test", float(1), qos=1)
+except KeyboardInterrupt:
+    print("goodbye...")
 
+finally:
 # Use disconnect() to disconnect from the broker.
-client.loop_stop()
-client.disconnect()
+    client.loop_stop()
+    client.disconnect()
--- Lab3/mulitplayerRPS.py
+++ Lab3/mulitplayerRPS.py
@@ -1,13 +1,21 @@
 import paho.mqtt.client as mqtt
+import numpy as np
 
-# Define callbacks - functions that run when events happen.
-counter = 0
+# THIS ACTS AS THE BROKER FOR THE INDIVIDUAL PLAYERS 
+brokerAddress = "host"
+brokerPort = 1883
+topic = "RPS"
+
+#storing the players moves
+name = "Enter name: "
+
+
 # Callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
     print("Connection returned result: " + str(rc))
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe("ece180d/test", qos=1)
+    client.subscribe(topic)
 
 # Callback of the client when it disconnects.
 def on_disconnect(client, userdata, rc):
@@ -16,16 +24,19 @@ def on_disconnect(client, userdata, rc):
     else:
         print('Expected Disconnect')
 
-# Default message callback.
-# (you can create separate callbacks per subscribed topic)
-def on_message(client, userdata, message):
-    global counter
-    print('Received message: "' + str(message.payload) + '" on topic "' +
-          message.topic + '" with QoS ' + str(message.qos))
-    counter += float(message.payload)
-    print("Counter: " + str(counter))
+def player_move ():
+    while True:
+        move = input("Rock [r], Paper [p], or Scissors [s] ?\n")
+        if move in ["r", "p", "s"]:
+            return move
+        else:
+            print("Invalid input! Rock [r], Paper [p], or Scissors [s] ?\n")
+
+
+
+
 # Create a client instance.
-client = mqtt.Client()
+client = mqtt.Client(name)
 
 # Add additional client options (security, certifications, etc.)
 # Many default options should be good to start off.
@@ -33,23 +44,23 @@ client = mqtt.Client()
 # Add callbacks to the client.
 client.on_connect = on_connect
 client.on_disconnect = on_disconnect
-client.on_message = on_message
 
 # Connect to a broker using one of the connect*() functions.
 # client.connect_async("test.mosquitto.org")
 client.connect_async('mqtt.eclipseprojects.io')
-# client.connect("test.mosquitto.org", 1883, 60)
-# client.connect("mqtt.eclipse.org")
 
 # Call one of the loop*() functions to maintain network traffic flow with the broker.
 client.loop_start()
-# client.loop_forever()
 
-while True:  # Perhaps add a stopping condition using some break or something.
-    pass  # Do your non-blocked other stuff here, like receive IMU data or something.
+try:
+    while True:
+        move = player_move()
+        client.publish(topic, f"{name},{move}")
 
-# Use subscribe() to subscribe to a topic and receive messages.
-# Use publish() to publish messages to the broker.
+except KeyboardInterrupt:
+    print("goodbye...")
+
+finally:
 # Use disconnect() to disconnect from the broker.
-client.loop_stop()
-client.disconnect()
+    client.loop_stop()
+    client.disconnect()
